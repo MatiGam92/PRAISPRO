0f4ba66674e42200b08e0d5845ebf08376545428
Mattgam <matigamarra92@gmail.com>
2025-12-08 22:50:14 -0300

    agregue favicon para pestania del navegador

--- resources/views/ia-team.blade.php
+++ resources/views/ia-team.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <title>Equipo de Desarrollo | PRAISPRO - IA-Team</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
