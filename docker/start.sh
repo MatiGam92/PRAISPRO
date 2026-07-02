dd1f9f99e1f1c48d6a7225f1e26aef86313e1612
Mattgam <matigamarra92@gmail.com>
2026-07-02 20:34:07 -0300

    Run migrations before cache commands

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+php artisan migrate --force
 php artisan optimize:clear
 php artisan package:discover --ansi
 php artisan storage:link --force || true
-php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
